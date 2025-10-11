9e2d0fb1efda41897b205e3043f4b5854fd8c5ff
simoncampos1022@gmail.com <simon.campos1022@gmail.com>
2025-10-10 21:15:43 -0500

    Removed: Gmail auth

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,49 +1,7 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/User');
-
-// Google OAuth Strategy
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:5000/api/auth/google/callback"
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-    // Check if user already exists
-    let user = await User.findOne({ googleId: profile.id });
-    
-    if (user) {
-      return done(null, user);
-    }
-
-    // Check if user exists with same email
-    user = await User.findOne({ email: profile.emails[0].value });
-    
-    if (user) {
-      // Link Google account to existing user
-      user.googleId = profile.id;
-      user.isVerified = true;
-      await user.save();
-      return done(null, user);
-    }
-
-    // Create new user
-    user = new User({
-      googleId: profile.id,
-      username: profile.displayName.replace(/\s+/g, '').toLowerCase() + Math.random().toString(36).substr(2, 4),
-      email: profile.emails[0].value,
-      isVerified: true,
-      password: null // No password for OAuth users
-    });
-
-    await user.save();
-    return done(null, user);
-  } catch (error) {
-    return done(error, null);
-  }
-}));
 
 // JWT Strategy
 passport.use(new JwtStrategy({
